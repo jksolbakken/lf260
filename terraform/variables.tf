aa459451d4eea2fda66307b18da804646cd2b4b6
J-K. Solbakken <jksolbakken@gmail.com>
2024-06-09 19:59:35 +0200

    specify gcp region and zone

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
-variable gcp_project {}
+variable gcp_project {
+    type = string
+}
 
 variable gcp_region {
     type = string
-    default = "europe-north1"
 }
 
 variable gcp_zone {
     type = string
-    default = "europe-north1-a"
 }
